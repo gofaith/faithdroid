d95c01d1d1477ecf928f99641dd12fc790e1bc45
StevenZack <yuhaozhou@alog.com>
2018-08-30 10:15:37 +0800

    update

--- main.go
+++ main.go
@@ -36,24 +36,31 @@ type MainActivity struct {
 	Activity
 }
 
+
 var strs = []string{
 	"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
 }
 
 func (m *MainActivity) OnCreate() {
 	a := &m.Activity
-	vlistview(a,
-		func(lv *FListView) IView {
-			return linearlayout(a).append(
-				button(a).setItemId(lv, "bt"),
-				textview(a).setItemId(lv, "txt"))
-		},
-		func(vh *ViewHolder, pos int) {
-			vh.getButtonByItemId("bt").text(strs[pos])
-			vh.getTextViewByItemId("txt").text(strs[pos])
-		},
-		func() int {
-			return len(strs)
-		}).show()
+	linearlayout(a).deferShow().append(
+		button(a).text("change").onClick(func() {
+			strs = strs[:3]
+			getListViewByItemId("lv").notifyDataSetChanged()
+		}),
+		vlistview(a,
+			func(lv *FListView) IView {
+				return linearlayout(a).append(
+					button(a).setItemId(lv, "bt"),
+					textview(a).setItemId(lv, "txt"))
+			},
+			func(vh *ViewHolder, pos int) {
+				vh.getButtonByItemId("bt").text(strs[pos])
+				vh.getTextViewByItemId("txt").text(strs[pos])
+			},
+			func() int {
+				return len(strs)
+			}).setId("lv"),
+	)
 }
 */
