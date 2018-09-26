02149232d9165cfeff23e40226a56c4a9c022bda
StevenZack <stevenzack@qq.com>
2018-09-26 13:24:25 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package faithdroid
 
 func (m *MainActivity) OnCreate() {
 	a := &m.Activity
-	LinearLayout(a).DeferShow().Append(
-		TextView(a).SetId("txt"),
-		Button(a).Text("check").OnClick(func() {
-			b := CheckSelfPermission(a, Permissions.READ_PHONE_STATE)
-			ShowToast(a, SPrintf(b))
+	LinearLayout(a).Append(
+		Button(a).Text("copy").SetId("cp").OnClick(func() {
+			Clipboard(a).ClipData("ok")
+			ShowToast(a, "copied")
 		}),
-		Button(a).Text("request").OnClick(func() {
-			RequestPermissions(a,
-				[]string{Permissions.CAMERA, Permissions.WRITE_EXTERNAL_STORAGE, Permissions.READ_PHONE_STATE},
-				func(bs []bool) {
-					ShowToast(a, JsonArray(bs))
-				})
-		}))
+	).Show()
 }
