d602909be4c33c8b32d204673fbd04d5cbf9ad34
StevenZack <stevenzack@qq.com>
2018-09-29 09:13:11 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package faithdroid
 
 func (a *MainActivity) OnCreate() {
-	LinearLayout(a).SetId("ctn").Size(-2, -2).Append(
-		Button(a).Text("add-1").Size(-1, -1).OnClick(func() {
-			GetLinearLayoutById("ctn").AddViewAt(
-				Button(a).Text("minus1").Size(-2, -1).LayoutGravity(Gravitys.Bottom),
-				-1,
-			)
-		}),
-		Button(a).Text("add-2").Size(-1, -1).OnClick(func() {
-			GetLinearLayoutById("ctn").AddViewAt(
-				Button(a).Text("minus2").Size(-2, -1).LayoutGravity(Gravitys.Bottom),
-				-2,
-			)
-		}),
+	LinearLayout(a).Size(-2, -2).Append(
+		TextView(a).Size(-2,-1).Text("hello").BackgroundColor()
 	).Show()
 }
