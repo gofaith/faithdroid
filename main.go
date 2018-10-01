70b8d97e73c23fd05c040e4c5e1b0a2b1d329e22
StevenZack <stevenzack@qq.com>
2018-10-01 13:57:35 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package faithdroid
 
 func (a *MainActivity) OnCreate() {
-	LinearLayout(a).Size(-2, -2).DeferShow().Append(
-		Button(a).Size(-2, -1).Text("standard").OnClick(func() {
-			StartActivity(a, func(a1 *Activity) {
-				name := a1.GetExtraValue("name")
-				TextView(a1).Text("standard activity by :" + name).Show()
-			}, NewActivityConfig().LaunchMode_Standard().PutExtra("name", "steven"))
+	LinearLayout(a).DeferShow().Size(-2, -2).Append(
+		TextView(a).SetId("text").Text("text view"),
+		Button(a).Size(-2, -1).Text("change text").OnClick(func() {
+			GetTextViewById("text").Text("text changed")
 		}))
 }
