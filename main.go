53073d9ead65f40d7bb72ce06c4d3fe3af1f53aa
StevenZack <stevenzack@qq.com>
2018-10-03 00:53:06 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package faithdroid
 
 func (a *MainActivity) OnCreate() {
 	LinearLayout(a).DeferShow().Size(-2, -2).Append(
-		TextView(a).SetId("text").Text("text view"),
-		Button(a).Size(-2, -1).Text("change text").OnClick(func() {
-			GetTextViewById("text").Text("text changed")
-		}))
+		ViewPager(a).Size(-2, -2).SetId("vp").LayoutWeight(1).Pages(
+			Page(func() IView {
+				return TextView(a).Text("page one")
+			}),
+			Page(func() IView {
+				return TextView(a).Text("page two")
+			})).OnPageSelected(func(i int) {
+			GetBottomNavById("bn").SelectedItem(i)
+		}),
+		BottomNav(a).SetId("bn").Size(-2, -1).Menus(
+			MenuItem("one").Icon(`drawable://add`).OnClick(func() {
+				GetViewPagerById("vp").CurrentItem(0, true)
+			}),
+			MenuItem("two").Icon(`drawable://add`).OnClick(func() {
+				GetViewPagerById("vp").CurrentItem(1, true)
+			})))
 }
