44e54c99d718eba60c188abc24fdee6b1af0241f
StevenZack <stevenzack@qq.com>
2018-10-08 17:23:25 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package faithdroid
 
 func (a *MainActivity) OnCreate() {
-	var data []string
-	var counter int
 	LinearLayout(a).Size(-2, -2).DeferShow().Append(
-		Button(a).Size(-2, -1).Text("add Item").OnClick(func() {
-			counter++
-			data = append(data, SPrintf(counter))
-			vl := GetListViewByItemId("vl")
-			vl.NotifyDataSetChanged()
-		}),
-		VListView(a,
-			func(lv *FListView) IView {
-				return LinearLayout(a).Size(-2, -1).Horizontal().Append(
-					TextView(a).TextColor(Colors.Red).SetItemId(lv, "title").Size(-1, -1),
-					Button(a).Size(-2, -1).SetItemId(lv, "bt"))
-			},
-			func(vh *ViewHolder, pos int) {
-				title := vh.GetTextViewByItemId("title")
-				title.Text(data[pos])
-				bt := vh.GetButtonByItemId("bt")
-				bt.Text(data[pos])
-			},
-			func() int {
-				return len(data)
-			},
-		).Size(-2, -2).SetId("vl"))
+		Toolbar(a).Size(-2, -1).Title("title").Menus(
+			MenuItem("add").Icon("drawable://add").ShowAsAction().OnClick(func() {
+				ShowToast(a, "search")
+			}),
+			MenuItem("chat"),
+			MenuItem("me"),
+			MenuItem("hello"),
+			SubMenu("settings",
+				MenuItem("set 1"),
+				MenuItem("set 2"))))
 }
