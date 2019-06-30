c09f2e9b2753b7ff50a1a2e4c9a6cb7d55467187
stevenzack <stevenzack@qq.com>
2019-06-30 15:08:59 +0800

    remove AttrSetGettable

--- android/app/src/main/java/io/github/gofaith/faithdroidapp/FViews/FButton.kt
+++ android/app/src/main/java/io/github/gofaith/faithdroidapp/FViews/FButton.kt
@@ -1,23 +1,30 @@
 package io.github.gofaith.faithdroidapp.FViews
 
 import androidx.appcompat.widget.AppCompatButton
-import io.github.gofaith.faithdroidapp.UI.AttrSetGettable
 import io.github.gofaith.faithdroidapp.UI.FView
 import io.github.gofaith.faithdroidapp.UI.UIKotlinBridge
 import io.github.gofaith.faithdroidapp.UI.Vars
 
-class FButton : FView,AttrSetGettable {
+class FButton : FView {
     val v:AppCompatButton
     constructor(vid:Long,ui:UIKotlinBridge) : super(Vars.button.ordinal,vid,ui) {
         v = AppCompatButton(ui.activity)
         view=v
     }
-    override fun setAttr(attr: Long, value: String) {
 
+    override fun setAttr(attr: Long, value: String): Boolean {
+        if(super.setAttr(attr, value)){
+            return true;
+        }
+        return false;
     }
 
-    override fun getAttr(attr: Long): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getAttr(attr: Long): String? {
+        var str = super.getAttr(attr)
+        if (str != null) {
+            return str;
+        }
 
+        return ""
+    }
 }
--- android/app/src/main/java/io/github/gofaith/faithdroidapp/UI/FView.kt
+++ android/app/src/main/java/io/github/gofaith/faithdroidapp/UI/FView.kt
@@ -4,4 +4,17 @@ import android.view.View
 
 open class FView(val className: Int, val vid:Long, val parentUIBridge:UIKotlinBridge) {
     var view: View? = null
+
+    open fun setAttr(attr: Long, value: String):Boolean {
+        when (attr) {
+
+        }
+        return false;
+    }
+    open fun getAttr(attr: Long):String? {
+        when (attr){
+
+        }
+        return null;
+    }
 }
